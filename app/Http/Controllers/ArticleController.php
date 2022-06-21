381e06b070eac765b428f2464815c96798e724c5
mukunzi10 <mukunzigustavus@gmail.com>
2022-06-21 01:53:38 -0700

    my progress1

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\article;
+use Illuminate\Support\Facades\Validator;
+use RealRashid\SweetAlert\Facades\Alert;
+use SweetAlert;
+
+class ArticleController extends Controller
+
+{
+
+    function   index()
+
+    {
+
+        $data=article::all()->getAlert();
+
+        return  $data;
+
+    }
+
+    function articleform(){
+
+        return  view('article');
+    }
+     
+function store(Request  $request)
+{
+
+    $AlertType='success';
+    $validator=Validator::make($request->all(),[
+        'title'=>'required|regex:/^[a-zA-Z\s]*$/',
+        'content'=>'required|string|min:10|max:350',
+        
+    ])->validate();
+  
+   
+$data=article::create([
+
+    'title'=>$request->title,
+    'content'=>$request->content,
+    'user_id'=>1
+]);
+Alert::toast('Article created successfully', 'success');
+return back();
+}
+
+}
+
+
+
+
