370d74042c4f9851ba15b97c6b9b2ecb4d9c9951
BISHOBOKERUWIZEYE Moise <bishomoise84@gmail.com>
2022-06-22 15:21:13 -0800

    Work finished

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -14,4 +14,9 @@ class Article extends Model
         'content',
         'user_id',
     ];
+
+    public function user()
+    {
+      return $this->belongsTo(User::class);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use App\Http\Controllers\UserController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth.login');
 });
 // Authonticated Middleware
 Route::middleware(['auth'])->group(function(){
